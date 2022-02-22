bea1fe47dfa04fd5a220710ef108046e536a8d67
NewTheBuilder <61889427+NewtheBuilder@users.noreply.github.com>
2022-02-22 19:31:40 +0100

    Add files via upload

---
+++ src/NewTheBuilder/Announce/Command/AnnounceCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace NewTheBuilder\Announce\Command;
+
+use NewTheBuilder\Announce\Main;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+
+class AnnounceCommand extends Command {
+
+    public function __construct() {
+        parent::__construct("announce", "Allows you to make an announcement on the server", "/announce <message>", ["say"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$sender->hasPermission("announce.use")){
+            $sender->sendMessage("§cYou don't ave permission to use this");
+            return true;
+        }
+        if (count($args) === 0) {
+            $sender->sendMessage("§cPlease post an ad");
+            return true;
+        }
+        $message = implode(" ", $args);
+        $config = new Config(Main::getInstance()->getDataFolder() . "Config.yml", Config::YAML);
+        Server::getInstance()->broadcastMessage($config->get("Prefix") . $config->get("Color") . $message);
+        return true;
+    }
+
+}
---
+++ src/NewTheBuilder/Announce/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NewTheBuilder\Announce;
+
+use JsonException;
+use NewTheBuilder\Announce\Command\AnnounceCommand;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener {
+
+    private static Main $main;
+
+    /**
+     * @return void
+     * @throws JsonException
+     */
+    protected function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        self::$main = $this;
+        $config = new Config($this->getDataFolder() . "Config.yml", Config::YAML, [
+            "Prefix" => "[§cANNOUNCE§f] ",
+            "Color" => "§e"
+        ]);
+        $config->save();
+        $this->getServer()->getCommandMap()->unregister($this->getCommand("say"));
+        $this->getServer()->getCommandMap()->register("Announce", new AnnounceCommand());
+    }
+
+    public static function getInstance() : Main {
+        return self::$main;
+    }
+
+}
